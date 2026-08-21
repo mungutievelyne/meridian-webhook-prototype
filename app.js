c268aa3b63e640df0f0e19e189ae7cdbc9bc0dd3
mungutievelyne <mungutievelyne3@gmail.com>
2026-08-21 22:33:34 +0300

    feat: receive POST request body

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ const http = require('http'); //Give me Node's built-in HTTP functionality.
 const server = http.createServer((req, res) => {
     console.log(req.method);
     console.log(req.url);
-      res.end('Hello!'); //Finish this response and send the text Hello! back to whoever made the request.
-     
+       
     
 if (req.method === 'POST') {
   console.log("Post received successfully");
+  let body = '';
+req.on('data', (chunk) => {
+    body += chunk;
+});
+
+  req.on('end', ( ) => {
+        console.log(body);
+    });
+
 }
+ res.end('Hello!'); //Finish this response and send the text Hello! back to whoever made the request.
+     
     });
 
 server.listen(3000);
